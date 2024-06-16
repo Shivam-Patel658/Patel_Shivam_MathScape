aca5d8844942fecc1d2cf8fd8d4b076ff5b7515b
shiva <shiva@DESKTOP-MANYA>
2024-06-15 21:23:38 -0400

    Add project files.

---
+++ Patel_Shivam_MathScape/Form1.Designer.vb
@@ -0,0 +1,294 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.pnlOptions = New System.Windows.Forms.Panel()
+        Me.pnlHelp = New System.Windows.Forms.Panel()
+        Me.pnlPlay = New System.Windows.Forms.Panel()
+        Me.lblLoad = New System.Windows.Forms.Label()
+        Me.lblNewGame = New System.Windows.Forms.Label()
+        Me.lblBack = New System.Windows.Forms.Label()
+        Me.pnlLoadingScreen = New System.Windows.Forms.Panel()
+        Me.pbLogo = New System.Windows.Forms.PictureBox()
+        Me.pbTextLogo = New System.Windows.Forms.PictureBox()
+        Me.pnlIntro = New System.Windows.Forms.Panel()
+        Me.pbIntro = New System.Windows.Forms.PictureBox()
+        Me.pnlMain = New System.Windows.Forms.Panel()
+        Me.lblOptions = New System.Windows.Forms.Label()
+        Me.lblHelp = New System.Windows.Forms.Label()
+        Me.lblPlay = New System.Windows.Forms.Label()
+        Me.pbKey = New System.Windows.Forms.PictureBox()
+        Me.pbTextLogoWhite = New System.Windows.Forms.PictureBox()
+        Me.tmrAnimation = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrLoading = New System.Windows.Forms.Timer(Me.components)
+        Me.pnlPlay.SuspendLayout()
+        Me.pnlLoadingScreen.SuspendLayout()
+        CType(Me.pbLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbTextLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.pnlIntro.SuspendLayout()
+        CType(Me.pbIntro, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.pnlMain.SuspendLayout()
+        CType(Me.pbKey, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbTextLogoWhite, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pnlOptions
+        '
+        Me.pnlOptions.BackColor = System.Drawing.Color.Transparent
+        Me.pnlOptions.Location = New System.Drawing.Point(12, 273)
+        Me.pnlOptions.Name = "pnlOptions"
+        Me.pnlOptions.Size = New System.Drawing.Size(30, 30)
+        Me.pnlOptions.TabIndex = 14
+        '
+        'pnlHelp
+        '
+        Me.pnlHelp.BackColor = System.Drawing.Color.Transparent
+        Me.pnlHelp.Location = New System.Drawing.Point(12, 227)
+        Me.pnlHelp.Name = "pnlHelp"
+        Me.pnlHelp.Size = New System.Drawing.Size(30, 30)
+        Me.pnlHelp.TabIndex = 15
+        '
+        'pnlPlay
+        '
+        Me.pnlPlay.BackColor = System.Drawing.Color.Transparent
+        Me.pnlPlay.Controls.Add(Me.lblLoad)
+        Me.pnlPlay.Controls.Add(Me.lblNewGame)
+        Me.pnlPlay.Controls.Add(Me.lblBack)
+        Me.pnlPlay.Location = New System.Drawing.Point(12, 174)
+        Me.pnlPlay.Name = "pnlPlay"
+        Me.pnlPlay.Size = New System.Drawing.Size(30, 30)
+        Me.pnlPlay.TabIndex = 19
+        '
+        'lblLoad
+        '
+        Me.lblLoad.AutoSize = True
+        Me.lblLoad.Font = New System.Drawing.Font("MARIO Font v3_2 Solid", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.lblLoad.ForeColor = System.Drawing.Color.White
+        Me.lblLoad.Location = New System.Drawing.Point(1030, 206)
+        Me.lblLoad.Name = "lblLoad"
+        Me.lblLoad.Size = New System.Drawing.Size(219, 83)
+        Me.lblLoad.TabIndex = 2
+        Me.lblLoad.Text = "LOAD"
+        '
+        'lblNewGame
+        '
+        Me.lblNewGame.AutoSize = True
+        Me.lblNewGame.Font = New System.Drawing.Font("MARIO Font v3_2 Solid", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.lblNewGame.ForeColor = System.Drawing.Color.White
+        Me.lblNewGame.Location = New System.Drawing.Point(166, 206)
+        Me.lblNewGame.Name = "lblNewGame"
+        Me.lblNewGame.Size = New System.Drawing.Size(408, 83)
+        Me.lblNewGame.TabIndex = 1
+        Me.lblNewGame.Text = "NEW GAME"
+        '
+        'lblBack
+        '
+        Me.lblBack.AutoSize = True
+        Me.lblBack.Font = New System.Drawing.Font("MARIO Font v3_2 Solid", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.lblBack.ForeColor = System.Drawing.Color.White
+        Me.lblBack.Location = New System.Drawing.Point(45, 19)
+        Me.lblBack.Name = "lblBack"
+        Me.lblBack.Size = New System.Drawing.Size(256, 83)
+        Me.lblBack.TabIndex = 0
+        Me.lblBack.Text = "<BACK"
+        '
+        'pnlLoadingScreen
+        '
+        Me.pnlLoadingScreen.Controls.Add(Me.pbLogo)
+        Me.pnlLoadingScreen.Controls.Add(Me.pbTextLogo)
+        Me.pnlLoadingScreen.Location = New System.Drawing.Point(12, 64)
+        Me.pnlLoadingScreen.Name = "pnlLoadingScreen"
+        Me.pnlLoadingScreen.Size = New System.Drawing.Size(30, 30)
+        Me.pnlLoadingScreen.TabIndex = 18
+        '
+        'pbLogo
+        '
+        Me.pbLogo.Image = Global.Patel_Shivam_MathScape.My.Resources.Brand.LOGO_PNG
+        Me.pbLogo.Location = New System.Drawing.Point(724, 39)
+        Me.pbLogo.Name = "pbLogo"
+        Me.pbLogo.Size = New System.Drawing.Size(176, 286)
+        Me.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pbLogo.TabIndex = 1
+        Me.pbLogo.TabStop = False
+        '
+        'pbTextLogo
+        '
+        Me.pbTextLogo.Image = Global.Patel_Shivam_MathScape.My.Resources.Brand.TEXT_LOGO
+        Me.pbTextLogo.Location = New System.Drawing.Point(300, 313)
+        Me.pbTextLogo.Name = "pbTextLogo"
+        Me.pbTextLogo.Size = New System.Drawing.Size(973, 535)
+        Me.pbTextLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pbTextLogo.TabIndex = 0
+        Me.pbTextLogo.TabStop = False
+        '
+        'pnlIntro
+        '
+        Me.pnlIntro.Controls.Add(Me.pbIntro)
+        Me.pnlIntro.Location = New System.Drawing.Point(12, 12)
+        Me.pnlIntro.Name = "pnlIntro"
+        Me.pnlIntro.Size = New System.Drawing.Size(30, 30)
+        Me.pnlIntro.TabIndex = 17
+        '
+        'pbIntro
+        '
+        Me.pbIntro.Image = Global.Patel_Shivam_MathScape.My.Resources.Intro.Intro_Animation
+        Me.pbIntro.Location = New System.Drawing.Point(260, 162)
+        Me.pbIntro.Name = "pbIntro"
+        Me.pbIntro.Size = New System.Drawing.Size(1080, 586)
+        Me.pbIntro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pbIntro.TabIndex = 0
+        Me.pbIntro.TabStop = False
+        '
+        'pnlMain
+        '
+        Me.pnlMain.BackColor = System.Drawing.Color.Transparent
+        Me.pnlMain.Controls.Add(Me.lblOptions)
+        Me.pnlMain.Controls.Add(Me.lblHelp)
+        Me.pnlMain.Controls.Add(Me.lblPlay)
+        Me.pnlMain.Controls.Add(Me.pbKey)
+        Me.pnlMain.Controls.Add(Me.pbTextLogoWhite)
+        Me.pnlMain.ForeColor = System.Drawing.Color.White
+        Me.pnlMain.Location = New System.Drawing.Point(12, 117)
+        Me.pnlMain.Name = "pnlMain"
+        Me.pnlMain.Size = New System.Drawing.Size(30, 30)
+        Me.pnlMain.TabIndex = 16
+        Me.pnlMain.Visible = False
+        '
+        'lblOptions
+        '
+        Me.lblOptions.AutoSize = True
+        Me.lblOptions.BackColor = System.Drawing.Color.Transparent
+        Me.lblOptions.Font = New System.Drawing.Font("MARIO Font v3_2 Solid", 71.99999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOptions.ForeColor = System.Drawing.Color.White
+        Me.lblOptions.Location = New System.Drawing.Point(576, 750)
+        Me.lblOptions.Name = "lblOptions"
+        Me.lblOptions.Size = New System.Drawing.Size(472, 124)
+        Me.lblOptions.TabIndex = 9
+        Me.lblOptions.Text = "Options"
+        '
+        'lblHelp
+        '
+        Me.lblHelp.AutoSize = True
+        Me.lblHelp.BackColor = System.Drawing.Color.Transparent
+        Me.lblHelp.Font = New System.Drawing.Font("MARIO Font v3_2 Solid", 71.99999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHelp.ForeColor = System.Drawing.Color.White
+        Me.lblHelp.Location = New System.Drawing.Point(659, 588)
+        Me.lblHelp.Name = "lblHelp"
+        Me.lblHelp.Size = New System.Drawing.Size(299, 124)
+        Me.lblHelp.TabIndex = 8
+        Me.lblHelp.Text = "Help"
+        '
+        'lblPlay
+        '
+        Me.lblPlay.AutoSize = True
+        Me.lblPlay.BackColor = System.Drawing.Color.Transparent
+        Me.lblPlay.Font = New System.Drawing.Font("MARIO Font v3_2 Solid", 71.99999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPlay.ForeColor = System.Drawing.Color.White
+        Me.lblPlay.Location = New System.Drawing.Point(659, 410)
+        Me.lblPlay.Name = "lblPlay"
+        Me.lblPlay.Size = New System.Drawing.Size(284, 124)
+        Me.lblPlay.TabIndex = 7
+        Me.lblPlay.Text = "Play"
+        '
+        'pbKey
+        '
+        Me.pbKey.BackColor = System.Drawing.Color.Transparent
+        Me.pbKey.Image = Global.Patel_Shivam_MathScape.My.Resources.Brand.LOGO_KEY
+        Me.pbKey.Location = New System.Drawing.Point(741, 26)
+        Me.pbKey.Name = "pbKey"
+        Me.pbKey.Size = New System.Drawing.Size(119, 71)
+        Me.pbKey.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pbKey.TabIndex = 6
+        Me.pbKey.TabStop = False
+        '
+        'pbTextLogoWhite
+        '
+        Me.pbTextLogoWhite.BackColor = System.Drawing.Color.Transparent
+        Me.pbTextLogoWhite.Image = Global.Patel_Shivam_MathScape.My.Resources.Brand.TEXT_LOGO_WHITE
+        Me.pbTextLogoWhite.Location = New System.Drawing.Point(140, 113)
+        Me.pbTextLogoWhite.Name = "pbTextLogoWhite"
+        Me.pbTextLogoWhite.Size = New System.Drawing.Size(1321, 229)
+        Me.pbTextLogoWhite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pbTextLogoWhite.TabIndex = 5
+        Me.pbTextLogoWhite.TabStop = False
+        '
+        'tmrAnimation
+        '
+        Me.tmrAnimation.Interval = 10
+        '
+        'tmrLoading
+        '
+        Me.tmrLoading.Interval = 10
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(1600, 900)
+        Me.Controls.Add(Me.pnlOptions)
+        Me.Controls.Add(Me.pnlHelp)
+        Me.Controls.Add(Me.pnlPlay)
+        Me.Controls.Add(Me.pnlLoadingScreen)
+        Me.Controls.Add(Me.pnlIntro)
+        Me.Controls.Add(Me.pnlMain)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "MathScape"
+        Me.pnlPlay.ResumeLayout(False)
+        Me.pnlPlay.PerformLayout()
+        Me.pnlLoadingScreen.ResumeLayout(False)
+        CType(Me.pbLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbTextLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.pnlIntro.ResumeLayout(False)
+        CType(Me.pbIntro, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.pnlMain.ResumeLayout(False)
+        Me.pnlMain.PerformLayout()
+        CType(Me.pbKey, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbTextLogoWhite, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pnlOptions As Panel
+    Friend WithEvents pnlHelp As Panel
+    Friend WithEvents pnlPlay As Panel
+    Friend WithEvents lblLoad As Label
+    Friend WithEvents lblNewGame As Label
+    Friend WithEvents lblBack As Label
+    Friend WithEvents pnlLoadingScreen As Panel
+    Friend WithEvents pbLogo As PictureBox
+    Friend WithEvents pbTextLogo As PictureBox
+    Friend WithEvents pnlIntro As Panel
+    Friend WithEvents pbIntro As PictureBox
+    Friend WithEvents pnlMain As Panel
+    Friend WithEvents lblOptions As Label
+    Friend WithEvents lblHelp As Label
+    Friend WithEvents lblPlay As Label
+    Friend WithEvents pbKey As PictureBox
+    Friend WithEvents pbTextLogoWhite As PictureBox
+    Friend WithEvents tmrAnimation As Timer
+    Friend WithEvents tmrLoading As Timer
+End Class
---
+++ Patel_Shivam_MathScape/Form1.vb
@@ -0,0 +1,296 @@
+﻿'{
+'  "APP METADATA.json": {
+'    "About Programmer": {
+'      "Name": "Patel, Shivam",
+'      "Email": "821658@pdsb.net"
+'    },
+'    "About Project": {
+'      "Title": "MathScape™",
+'      "Course": "ICD2O0-B",
+'      "Instructor": "A, Simler",
+'      "Published": "14 June 2024"
+'    }
+'    "Additional Information on current file": {
+'      "Name": "Form1.vb",
+'      "Description": "Main Menu of Video Game"
+'    }
+
+'     "Attributions" {
+'       "Game TEXT Logo" "https://www.textstudio.com/"
+'        "Intro MUSIC": "Supercell"
+'        "Main Menu Theme": "NPT Music"
+
+'    }
+'  }
+'}
+
+
+
+Imports System.IO
+Imports System.Media
+Imports System.Resources
+Imports System.Runtime.CompilerServices
+Imports System.Windows.Media
+Imports System.Reflection
+
+Module ControlExtensions
+
+    <Extension()> Public Sub Hispose(ctrl As Control)
+
+        If ctrl IsNot Nothing Then
+
+            ctrl.Hide()
+            ctrl.Dispose()
+
+        End If
+
+    End Sub
+
+End Module
+
+
+Public Class frmMain
+
+
+    'Declaring neccesary variables
+    Dim tickIntro As Decimal
+    Dim tickLoading As Decimal
+
+    Private fadeOpacity As Decimal
+    Private fadeIn As Boolean = True ' To control fade in and fade out
+
+    Public soundPlayer As New SoundPlayer()                                                 'SoundPlayer variable to play audio (slightly complex method but allows for resource files)
+    Public bgmPlayer As New MediaPlayer()
+
+    Private tempFilePath As String
+
+    Sub PlayMusic(ByVal resourceFile As Object, ByVal resourceName As String)
+        ' Create a new SoundPlayer
+        Dim soundPlayer As New SoundPlayer()
+
+        ' Retrieve the resource stream using the specified resource file and resource name
+        Dim resourceManager = resourceFile.ResourceManager
+        Dim resourceStream As IO.Stream = resourceManager.GetStream(resourceName)
+
+        If resourceStream IsNot Nothing Then
+            ' Assign the stream to the SoundPlayer
+            soundPlayer.Stream = resourceStream        ' Play the sound
+            soundPlayer.Play()
+        Else
+            ' Handle the case where the resource is not found
+            Throw New Exception("Resource not found: " & resourceName)
+        End If
+
+    End Sub
+
+
+
+    Sub Pre()
+
+        Me.BackgroundImage = Nothing
+
+        Dim panels() As Panel = {pnlMain, pnlIntro, pnlLoadingScreen, pnlPlay, pnlHelp, pnlOptions}
+
+        For Each panel As Panel In panels
+
+            panel.Hide()
+            panel.Location = New Point(0, 0)
+            panel.Size = New Size(1600, 900)
+
+        Next
+
+
+
+    End Sub
+
+    'Game intro (first thing when game is opened)
+
+    Sub PlaySound(ByVal resourceFileName As String, ByVal resourceName As String)
+        ' Create a new SoundPlayer
+        Dim soundPlayer As New SoundPlayer()
+
+        ' Use reflection to get the ResourceManager from the specified resource file name
+        Dim resourceManager As ResourceManager
+        Try
+            ' Get the type of the resource file from the assembly
+            Dim resourceFileType As Type = Assembly.GetExecutingAssembly().GetType("Patel_Shivam_MathScape.My.Resources." & resourceFileName)
+            If resourceFileType Is Nothing Then
+                Throw New Exception("Resource file not found: " & resourceFileName)
+            End If
+
+            ' Get the ResourceManager property from the resource file type
+            Dim resourceManagerProperty As PropertyInfo = resourceFileType.GetProperty("ResourceManager", BindingFlags.Static Or BindingFlags.NonPublic)
+            If resourceManagerProperty Is Nothing Then
+                Throw New Exception("ResourceManager property not found in resource file: " & resourceFileName)
+            End If
+
+            ' Get the ResourceManager instance
+            resourceManager = CType(resourceManagerProperty.GetValue(Nothing, Nothing), ResourceManager)
+        Catch ex As Exception
+            Throw New Exception("Failed to get ResourceManager for resource file: " & resourceFileName, ex)
+        End Try
+
+        ' Retrieve the resource stream using the resource manager and resource name
+        Dim resourceStream As IO.Stream = resourceManager.GetStream(resourceName)
+
+        If resourceStream IsNot Nothing Then
+            ' Assign the stream to the SoundPlayer
+            soundPlayer.Stream = resourceStream
+            ' Play the sound
+            soundPlayer.Play()
+        Else
+            ' Handle the case where the resource is not found
+            Throw New Exception("Resource not found: " & resourceName)
+        End If
+
+    End Sub
+
+    Sub Intro()
+
+        tmrAnimation.Start()
+        pnlIntro.Show()
+        PlaySound("Audio", "Intro_Music")
+
+        'Throw New ArgumentException("Exception Occured")
+
+
+    End Sub
+
+    'Game Loading Screen (after intro)
+    Sub LoadingScreen()
+
+        tmrLoading.Start()
+        pnlLoadingScreen.Show()
+        PlaySound("Audio", "LOADING_SOUND")
+
+
+    End Sub
+
+
+
+    'Game MainMenu (After fade)
+    Sub MainMenu()
+
+        Me.BackgroundImage = My.Resources.Intro.BACKGROUND
+
+        pnlMain.Show()
+
+        'tempFilePath = Path.GetTempFileName()
+        'File.WriteAllBytes(tempFilePath, My.Resources.Audio.Main_Menu_Theme)
+        'MediaPlayer.Open(New Uri(tempFilePath))
+        'bgmPlayer.Play()
+
+        'PlaySound("Audio", "Main_Menu_Theme")
+
+
+    End Sub
+
+
+    'Form Load Event
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        Pre()
+        Intro()                                                                      'Runs intro sub, no call keyword required, no parenthesis required as there are no parameters
+
+
+    End Sub
+
+
+    Private Sub Label_Click(sender As Object, e As EventArgs) Handles lblPlay.Click, lblHelp.Click, lblOptions.Click
+
+        Dim lbl As Label = CType(sender, Label)
+        Dim panelToShow As Panel = Nothing
+
+        Select Case lbl.Name
+
+            Case "lblPlay"
+                panelToShow = pnlPlay
+
+            Case "lblHelp"
+                panelToShow = pnlHelp
+
+            Case "lblOptions"
+                panelToShow = pnlOptions
+
+        End Select
+
+
+        panelToShow.SuspendLayout()
+
+        panelToShow.Show()
+        panelToShow.Controls.Add(lblBack)
+
+        panelToShow.ResumeLayout()
+
+
+    End Sub
+
+
+    'Timer Tick Event
+    Private Sub tmrIntro_Tick(sender As Object, e As EventArgs) Handles tmrAnimation.Tick
+
+        tickIntro += 0.01
+
+        If tickIntro = 1 Then
+
+            pnlIntro.Hispose()
+            LoadingScreen()
+
+
+        End If
+
+    End Sub
+
+
+    'Timer Tick Event
+    Private Sub tmrLoading_Tick(sender As Object, e As EventArgs) Handles tmrLoading.Tick
+
+        tickLoading += 0.01
+
+        If tickLoading = 1.0 Then
+
+            pbLogo.Size = New Point(294, 306)
+            pbLogo.Location = New Point(523 + 142, 29)
+            pbLogo.Image = My.Resources.Brand.LOGO_LIGHT
+
+        ElseIf tickLoading = 2 Then
+
+            pnlLoadingScreen.Hide()
+            MainMenu()
+
+        End If
+
+    End Sub
+
+
+
+
+    Private Sub lblBack_Click(sender As Object, e As EventArgs) Handles lblBack.Click
+
+        Dim strParentName As String = lblBack.Parent.Name
+
+        pnlMain.Show()
+
+        '**Allows me to use one "<back" label for all panels
+        Select Case strParentName
+
+            Case "pnlPlay"
+                pnlPlay.Hide()
+                pnlPlay.Controls.Remove(lblBack)
+
+            Case "pnlHelp"
+                pnlHelp.Hide()
+                pnlHelp.Controls.Remove(lblBack)
+
+            Case "pnlOptions"
+                pnlOptions.Hide()
+                pnlOptions.Controls.Remove(lblBack)
+
+        End Select
+
+        strParentName = Nothing
+
+    End Sub
+
+End Class
